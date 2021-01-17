10331ad2cb7dfbe1259fee9ad52d5e4d483a2eb6
flavienfr <flav.rsl@hotmail.fr>
2021-01-17 16:55:32 +0100

    build dront

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 
-// Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
-// App
 const app = express();
+
+// Cors for frontend ip != backend ip
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World 2');
 });
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiUrl = `http://localhost:8080`;
+
+class App extends Component {
+  state = {
+    data: "empty"
+  };
+
+  async loadData() {
+    const res = await axios.get(apiUrl + '/');
+    this.setState({
+      data: res.data
+    });
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Api data from root request: {this.state.data}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
